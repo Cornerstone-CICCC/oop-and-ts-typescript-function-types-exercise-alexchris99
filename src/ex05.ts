e3a0802a9ad4ab720df911f23f7e5b5102a516f7
Christian Jimenez <cajt@outlook.com>
2025-02-06 12:20:09 -0800

    All done

--- src/ex05.ts
+++ src/ex05.ts
@@ -2,8 +2,12 @@
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
 
-function sumAllNumbers( ) {
-
+function sumAllNumbers(...numbers: number[]) {
+    let total = 0 
+    numbers.forEach(number =>{
+        total += number
+    })
+    return total
 }
 
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
--- src/ex10.ts
+++ src/ex10.ts
@@ -6,7 +6,8 @@ function createUser(name: string, age: number): { name: string, age: number } {
   return { name, age };
 }
 
-const myCreateUser = function(name, age) {
+const myCreateUser: typeof createUser = function(name, age):{name:string, age: number} {
+  return {name,age}
   
 }
 
